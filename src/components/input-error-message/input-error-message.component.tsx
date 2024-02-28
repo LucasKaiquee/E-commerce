95a46e83fcd774d0572f800d9fc5d8d1bfa52bf9
LucasKaiquee <lucaskaique743@gmail.com>
2024-02-27 21:37:43 -0300

    feat: add Input Error Message component

---
+++ src/components/input-error-message/input-error-message.component.tsx
@@ -0,0 +1,16 @@
+import { FunctionComponent } from "react"
+import { InputErrorMessageContainer } from "./input-error-message.styles"
+
+interface InputErrorMessageProps {
+    children: string
+}
+
+const InputErrorMessage: FunctionComponent<InputErrorMessageProps> = ({children}) => {
+    return (
+        <InputErrorMessageContainer>
+            {children}
+        </InputErrorMessageContainer>
+    )
+}
+
+export default InputErrorMessage
---
+++ src/components/input-error-message/input-error-message.styles.ts
@@ -0,0 +1,9 @@
+import Colors from "@/theme/theme.colors";
+import styled from "styled-components";
+
+export const InputErrorMessageContainer = styled.p`
+    width: 100%;
+    color: ${Colors.error};
+    font-size: 0%.75rem;
+    margin-top: 5px;
+`
