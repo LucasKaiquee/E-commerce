a63bbbe04b87b2896635856b7dd654d15c73bd49
LucasKaiquee <lucaskaique743@gmail.com>
2024-02-27 21:09:24 -0300

    feat: add React Hook Form validation to Login Page and Custom Input

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,36 +7,63 @@ import { LoginContainer, LoginContent, LoginHeadline, LoginInputContainer, Login
 
 import { BsGoogle } from "react-icons/bs"
 import { FiLogIn } from "react-icons/fi"
+import { useForm } from "react-hook-form"
+
+
 import CustomInput from "@/components/custom-input/custom-input.component"
 
 export default function LoginPage() {
+
+    const {
+        register,
+        formState: { errors },
+        handleSubmit
+    } = useForm()
+
+    const handleSubmitPress = (data: any) => {
+        
+    }
 
     return (
         <>
             <Header />
             <LoginContainer>
                 <LoginContent>
-
                     <LoginHeadline>Entre com a sua conta</LoginHeadline>
 
-                    <CustomButton startIcon={<BsGoogle size={20}/>}>Entar com o google</CustomButton>
+                    <CustomButton 
+                        startIcon={<BsGoogle size={20} />}>
+                            Entar com o google
+                    </CustomButton>
 
                     <LoginSubtitle>ou entre com  seu e-mail</LoginSubtitle>
 
                     <LoginInputContainer>
                         <p>Email</p>
-                        <CustomInput placeholder="Digite seu e-mail" />
+                        <CustomInput 
+                            hasError={!!errors?.email}
+                            placeholder="Digite seu e-mail" 
+                            {...register('email', { required: true })} 
+                        />
                     </LoginInputContainer>
 
                     <LoginInputContainer>
                         <p>Senha</p>
-                        <CustomInput placeholder="Digite sua senha" />
+                        <CustomInput 
+                            hasError={!!errors?.password}
+                            placeholder="Digite sua senha" 
+                            {...register('password', { required: true })} 
+                        />
                     </LoginInputContainer>
 
-                    <CustomButton startIcon={<FiLogIn size={20}/>}>Entrar</CustomButton>
+                    <CustomButton 
+                        startIcon={<FiLogIn size={20} />} 
+                        onClick={() => handleSubmit(handleSubmitPress)()}>
+                        Entrar
+                    </CustomButton>
+
                 </LoginContent>
             </LoginContainer>
         </>
-
     )
 }
